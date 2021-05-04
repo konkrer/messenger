81b7e607cf828bb135b254a4d5dacad1bac9a197
konkrer <rivortex@gmail.com>
2021-05-04 11:23:44 -0700

    token in HttpOnly cookie

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
-import React from "react";
-import { MuiThemeProvider } from "@material-ui/core";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./store";
+import React from 'react';
+import { MuiThemeProvider } from '@material-ui/core';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
-import { theme } from "./themes/theme";
-import Routes from "./routes";
+// local
+import store from './store';
+import { theme } from './themes/theme';
+import Routes from './routes';
 
 function App() {
   return (
