e3f74442499a6a9319c0f734e2f9c95743bdafdc
konkrer <rivortex@gmail.com>
2021-05-05 09:23:19 -0700

    send msg bug fixed, msg order fixed, new msg scrll

--- client/src/components/ActiveChat/Input.js
+++ client/src/components/ActiveChat/Input.js
@@ -1,17 +1,17 @@
-import React, { Component } from "react";
-import { FormControl, FilledInput } from "@material-ui/core";
-import { withStyles } from "@material-ui/core/styles";
-import { connect } from "react-redux";
-import { postMessage } from "../../store/utils/thunkCreators";
+import React, { Component } from 'react';
+import { FormControl, FilledInput } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import { connect } from 'react-redux';
+import { postMessage } from '../../store/utils/thunkCreators';
 
 const styles = {
   root: {
-    justifySelf: "flex-end",
+    justifySelf: 'flex-end',
     marginTop: 15,
   },
   input: {
     height: 70,
-    backgroundColor: "#F4F6FA",
+    backgroundColor: '#F4F6FA',
     borderRadius: 8,
     marginBottom: 20,
   },
@@ -21,17 +21,17 @@ class Input extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: "",
+      text: '',
     };
   }
 
-  handleChange = (event) => {
+  handleChange = event => {
     this.setState({
       text: event.target.value,
     });
   };
 
-  handleSubmit = async (event) => {
+  handleSubmit = async event => {
     event.preventDefault();
     // add sender user info if posting to a brand new convo, so that the other user will have access to username, profile pic, etc.
     const reqBody = {
@@ -42,14 +42,14 @@ class Input extends Component {
     };
     await this.props.postMessage(reqBody);
     this.setState({
-      text: "",
+      text: '',
     });
   };
 
   render() {
-    const { classes } = this.props;
+    const { classes, id } = this.props;
     return (
-      <form className={classes.root} onSubmit={this.handleSubmit}>
+      <form className={classes.root} onSubmit={this.handleSubmit} id={id}>
         <FormControl fullWidth hiddenLabel>
           <FilledInput
             classes={{ root: classes.input }}
@@ -65,16 +65,16 @@ class Input extends Component {
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     user: state.user,
     conversations: state.conversations,
   };
 };
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
-    postMessage: (message) => {
+    postMessage: message => {
       dispatch(postMessage(message));
     },
   };
